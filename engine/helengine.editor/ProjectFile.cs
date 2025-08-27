f220b7eaa02ba27fd50947de92ec962e37a049c3
helen <lucasassis@outlook.com>
2025-08-27 11:09:12 -0300

    Helengine

---
+++ engine/helengine.editor/ProjectFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helengine.editor {
+    public class ProjectFile {
+        public string ProjectName { get; set; }
+    }
+}
